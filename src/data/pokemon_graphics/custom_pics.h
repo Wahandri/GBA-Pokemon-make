27af030c9380e9d116244a37e02d7bade8a5b3cc
Manuel García Cobos <115196757+Wahandri@users.noreply.github.com>
2025-10-19 04:57:22 +0200

    Integrate custom sprites for Gorda and Charly

---
+++ src/data/pokemon_graphics/custom_pics.h
@@ -0,0 +1,7 @@
+const u32 gMonFrontPic_Gorda[] = INCBIN_U32("graphics/pokemon/gorda/front.4bpp.lz");
+const u32 gMonBackPic_Gorda[] = INCBIN_U32("graphics/pokemon/gorda/back.4bpp.lz");
+const u32 gMonPalette_Gorda[] = INCBIN_U32("graphics/pokemon/gorda/normal.gbapal.lz");
+
+const u32 gMonFrontPic_Charly[] = INCBIN_U32("graphics/pokemon/charly/front.4bpp.lz");
+const u32 gMonBackPic_Charly[] = INCBIN_U32("graphics/pokemon/charly/back.4bpp.lz");
+const u32 gMonPalette_Charly[] = INCBIN_U32("graphics/pokemon/charly/normal.gbapal.lz");
--- src/data/pokemon_graphics/pokemon_graphics.h
+++ src/data/pokemon_graphics/pokemon_graphics.h
@@ -1,20 +1,20 @@
 #ifndef GUARD_DATA_POKEMON_GRAPHICS_POKEMON_GRAPHICS_H
 #define GUARD_DATA_POKEMON_GRAPHICS_POKEMON_GRAPHICS_H
 
-// --- GORDA ---
 extern const u32 gMonFrontPic_Gorda[];
 extern const u32 gMonBackPic_Gorda[];
-extern const u16 gMonPalette_Gorda[];
-extern const u16 gMonShinyPalette_Gorda[];
+extern const u32 gMonPalette_Gorda[];
 extern const u8  gMonIcon_Gorda[];
+#if P_FOOTPRINTS
 extern const u8  gMonFootprint_Gorda[];
+#endif
 
-// --- CHARLY ---
 extern const u32 gMonFrontPic_Charly[];
 extern const u32 gMonBackPic_Charly[];
-extern const u16 gMonPalette_Charly[];
-extern const u16 gMonShinyPalette_Charly[];
+extern const u32 gMonPalette_Charly[];
 extern const u8  gMonIcon_Charly[];
+#if P_FOOTPRINTS
 extern const u8  gMonFootprint_Charly[];
+#endif
 
 #endif // GUARD_DATA_POKEMON_GRAPHICS_POKEMON_GRAPHICS_H
